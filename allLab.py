5f36cc26c34d139c1d13624c9323a897d40e6697
Shoaib Ahmad <57339978+shoaibahmadsakib@users.noreply.github.com>
2022-09-14 08:17:18 +0600

    Update allLab.py

--- allLab.py
+++ allLab.py
@@ -371,3 +371,45 @@ q.display()
 q.dequeue()
 print("Dequeue elements:")
 q.display()
+
+
+
+
+
+
+
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+
+    # Add an element
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    # Remove an element
+    def dequeue(self):
+        if len(self.queue) < 1:
+            return None
+        return self.queue.pop(0)
+
+    # Display  the queue
+    def display(self):
+        print(self.queue)
+
+    def size(self):
+        return len(self.queue)
+
+
+q = Queue()
+n = int(input("Enter the number of elements for queue : "))
+print("Enter the elements :")
+for i in range(n):
+    q.enqueue(int(input()))
+print("Queue elements:")
+q.display()
+
+q.dequeue()
+
+print("Dequeue elements:")
+q.display()
